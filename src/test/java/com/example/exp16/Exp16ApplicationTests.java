6fa96fa23fdaa6282efcd628922b9cac713a6368
mateen <mateennandgadi@gmail.com>
2024-11-15 11:59:01 +0530

    message

---
+++ src/test/java/com/example/exp16/Exp16ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.exp16;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class Exp16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/example/exp16/Test.java
@@ -0,0 +1,5 @@
+package com.example.exp16;
+
+public @interface Test {
+
+}
